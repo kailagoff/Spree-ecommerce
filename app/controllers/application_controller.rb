a8421698ae90fb9aa3c428a318d5c320fee16249
kailagoff <kaila.goff@gmail.com>
2019-09-03 17:40:13 -0700

    add helpers to get cart working on all pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  include Spree::Core::ControllerHelpers::Store
+  include Spree::Core::ControllerHelpers::Order
+  include Spree::Core::ControllerHelpers::Auth
 end
