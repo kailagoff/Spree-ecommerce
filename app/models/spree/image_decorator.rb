cbdc7bf74e19654bf6e7a1c8986d680abd22dc3f
kailagoff <kaila.goff@gmail.com>
2019-09-12 21:43:08 -0700

    add image decorator model, adjust product pictures

---
+++ app/models/spree/image_decorator.rb
@@ -0,0 +1,22 @@
+module YourApplication
+  module Spree
+    module ImageDecorator
+      module ClassMethods
+        def styles
+          {
+            mini:    '48x48>',
+            small:   '100x100>',
+            product: '250x250>',
+            large:   '400x400>',
+          }
+        end
+      end
+
+      def self.prepended(base)
+        base.singleton_class.prepend ClassMethods
+      end
+    end
+  end
+end
+
+Spree::Image.prepend ::YourApplication::Spree::ImageDecorator
